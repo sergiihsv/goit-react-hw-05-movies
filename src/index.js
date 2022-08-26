181f0f16a16b92c66297c212b2f1eac64fae592e
sergiihsv <hsv-serg@ukr.net>
2022-08-26 10:02:35 +0300

    fix deploy

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { theme } from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
       <ThemeProvider theme={theme}>
         <App />
         <ToTop />
